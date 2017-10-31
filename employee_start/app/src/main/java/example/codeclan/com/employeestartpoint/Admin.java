d9728043ea8382cf5741ee9d8eb0eca4ccc84674
Miguel <miguelmoutela@hotmail.com>
2017-10-31 18:29:20 +0000

    created Director

--- employee_start/app/src/main/java/example/codeclan/com/employeestartpoint/Admin.java
+++ employee_start/app/src/main/java/example/codeclan/com/employeestartpoint/Admin.java
@@ -4,5 +4,9 @@ package example.codeclan.com.employeestartpoint;
  * Created by user on 31/10/2017.
  */
 
-public class Admin {
+public class Admin extends Employee{
+
+    public Admin(int id, String name, String socialSecurityNumber, double salary) {
+        super(id, name, socialSecurityNumber, salary);
+    }
 }
--- employee_start/app/src/main/java/example/codeclan/com/employeestartpoint/Engineer.java
+++ employee_start/app/src/main/java/example/codeclan/com/employeestartpoint/Engineer.java
@@ -4,5 +4,8 @@ package example.codeclan.com.employeestartpoint;
  * Created by user on 31/10/2017.
  */
 
-public class Engineer {
+public class Engineer extends Employee {
+    public Engineer(int id, String name, String socialSecurityNumber, double salary) {
+        super(id, name, socialSecurityNumber, salary);
+    }
 }
